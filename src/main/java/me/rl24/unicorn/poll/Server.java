c80b899d8ca0306388ffe0ce9918ffe68048a732
RL24 <richardlapwood24@gmail.com>
2020-01-21 01:10:45 +1300

    test

--- src/main/java/me/rl24/unicorn/poll/Server.java
+++ src/main/java/me/rl24/unicorn/poll/Server.java
@@ -6,6 +6,9 @@ import me.rl24.unicorn.poll.router.Router;
 import me.rl24.unicorn.poll.util.EnvironmentHelper;
 import org.reflections.Reflections;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
@@ -14,6 +17,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 
+@Controller
 @SpringBootApplication
 public class Server implements EnvironmentHelper {
 
@@ -21,28 +25,33 @@ public class Server implements EnvironmentHelper {
     private static final ExecutorService EXECUTOR = Executors.newFixedThreadPool(4);
 
     public Server() throws IOException {
-        LOG.info("Starting server...");
-        HttpServer server = HttpServer.create(new InetSocketAddress("localhost", getPort()), 0);
-        server.setExecutor(EXECUTOR);
+//        LOG.info("Starting server...");
+//        HttpServer server = HttpServer.create(new InetSocketAddress("localhost", getPort()), 0);
+//        server.setExecutor(EXECUTOR);
+//
+//        LOG.info("Creating routers...");
+//        new Reflections(Server.class.getPackage().getName()).getSubTypesOf(HttpHandler.class).forEach((cls) -> {
+//            try {
+//                Router router = cls.getAnnotation(Router.class);
+//                if (router != null) {
+//                    HttpHandler instance = (HttpHandler) cls.getConstructors()[0].newInstance();
+//                    server.createContext(router.value(), instance);
+//                    LOG.info(String.format("Created route for '%s'", router.value()));
+//                }
+//            } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+//                LOG.severe(e.getMessage());
+//                e.printStackTrace();
+//                System.exit(0);
+//            }
+//        });
+//
+//        server.start();
+//        LOG.info("Listening on port " + getPort());
+    }
 
-        LOG.info("Creating routers...");
-        new Reflections(Server.class.getPackage().getName()).getSubTypesOf(HttpHandler.class).forEach((cls) -> {
-            try {
-                Router router = cls.getAnnotation(Router.class);
-                if (router != null) {
-                    HttpHandler instance = (HttpHandler) cls.getConstructors()[0].newInstance();
-                    server.createContext(router.value(), instance);
-                    LOG.info(String.format("Created route for '%s'", router.value()));
-                }
-            } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-                LOG.severe(e.getMessage());
-                e.printStackTrace();
-                System.exit(0);
-            }
-        });
-
-        server.start();
-        LOG.info("Listening on port " + getPort());
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    private String index() {
+        return "yes";
     }
 
 }
